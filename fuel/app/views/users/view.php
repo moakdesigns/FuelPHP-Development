95e145531e8b8967ba2c7cfee2e76a306bfc535b
moakdesigns <moakdesigns@gmail.com>
2014-02-18 11:07:54 -0500

    Revert "scaffold users"
    This reverts commit 7651e041b3e74058f99a4735061a0b2a16ab9ed5.

--- fuel/app/views/users/view.php
+++
@@ -1,5 +0,0 @@
-<h2>Viewing <span class='muted'>#<?php echo $user->id; ?></span></h2>
-
-
-<?php echo Html::anchor('users/edit/'.$user->id, 'Edit'); ?> |
-<?php echo Html::anchor('users', 'Back'); ?>
