40f8e7b505bc5253e79d71d287fad1027f40e236
moakdesigns <moakdesigns@gmail.com>
2014-02-20 11:36:34 -0500

    Revert "created users"
    This reverts commit 04baf74d36dcc0981f2a990c0e9bb459461ceda3.

--- fuel/app/views/admin/user/create.php
+++
@@ -1,7 +0,0 @@
-<h2>New User</h2>
-<br>
-
-<?php echo render('admin\user/_form'); ?>
-
-
-<p><?php echo Html::anchor('admin/user', 'Back'); ?></p>
--- fuel/app/views/admin/user/edit.php
+++
@@ -1,7 +0,0 @@
-<h2>Editing User</h2>
-<br>
-
-<?php echo render('admin\user/_form'); ?>
-<p>
-	<?php echo Html::anchor('admin/user/view/'.$user->id, 'View'); ?> |
-	<?php echo Html::anchor('admin/user', 'Back'); ?></p>
