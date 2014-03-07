0be1048201b126aa4fafc7ffa920e47453495672
Jenna Moakley <moakdesigns@gmail.com>
2014-03-07 15:05:56 -0500

    migrations

--- fuel/app/config/development/migrations.php
+++ fuel/app/config/development/migrations.php
@@ -4,7 +4,24 @@ return array (
   array (
     'app' => 
     array (
-      'default' => 0,
+      'default' => 
+      array (
+        0 => '001_create_users',
+        1 => '002_create_users_clients',
+        2 => '003_create_users_groups',
+        3 => '004_create_users_group_permissions',
+        4 => '005_create_users_group_roles',
+        5 => '006_create_users_metadatas',
+        6 => '007_create_users_permissions',
+        7 => '008_create_users_providers',
+        8 => '009_create_users_roles',
+        9 => '010_create_users_role_permissions',
+        10 => '011_create_users_scopes',
+        11 => '012_create_users_sessions',
+        12 => '013_create_users_sessionscopes',
+        13 => '014_create_users_user_permissions',
+        14 => '015_create_users_user_roles',
+      ),
     ),
     'module' => 
     array (
