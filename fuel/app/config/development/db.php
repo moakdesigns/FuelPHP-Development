b5e6f6e34477d476b3d669ebb3077c706ca2ca96
moakdesigns <moakdesigns@gmail.com>
2014-03-12 16:27:15 -0400

    Revert "created messages"
    This reverts commit b6345ca0cbe5f8ccd48a7d5e38c13c0bdbc85bc2.

--- fuel/app/config/development/db.php
+++ fuel/app/config/development/db.php
@@ -6,7 +6,7 @@
 return array(
 	'default' => array(
 		'connection'  => array(
-			'dsn'        => 'mysql:host=localhost;dbname=crash_course',
+			'dsn'        => 'mysql:host=localhost;dbname=fuelphp_dev',
 			'username'   => 'root',
 			'password'   => 'snc2014',
 		),
--- fuel/app/views/messages/create.php
+++
@@ -1,7 +0,0 @@
-<h2>New <span class='muted'>Message</span></h2>
-<br>
-
-<?php echo render('messages/_form'); ?>
-
-
-<p><?php echo Html::anchor('messages', 'Back'); ?></p>
